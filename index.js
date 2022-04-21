e5b0c38307e5a90ea17d2dac71356bd19f963c73
Ruslan <noriod@yandex.ru>
2022-04-21 15:48:05 +0300

    refactor

--- index.js
+++ index.js
@@ -4,25 +4,67 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = '5175773703:AAG9ntBfob88aEKIe5jUHXbpDBKVVqsYQ-c';
 const bot = new TelegramBot(token, {polling: true});
-const chatId = 1039025815;
-let threshold = 86;
 const refreshTimeInMinutes = 2;
 
-function checkEUR() {
+const chatsAndRates = {};
+
+bot.setMyCommands([
+  {command: '/currentrate', description: 'Получить текущий курс.'},
+  {command: '/start', description: 'Запускает бота, обнуляет курс.'},
+]);
+
+const setExchangeRate = async (chatId, text) => {
+  chatsAndRates[chatId] = text;
+  await bot.sendMessage(chatId, `Вы установили курс: ${text} рублей.`);
+}
+
+const getCurrentRate = async () => {
   try {
-    axios.get('https://api.tinkoff.ru/v1/currency_rates?from=EUR&to=RUB')
-    .then(response => {
-      const eurRate = response.data.payload.rates[5].buy;
-      if (eurRate >= threshold) {
-        bot.sendMessage(chatId, 'TIME TO SELL! Exchange rate is ' + eurRate);
-      } else {
-        console.log(eurRate);
-      }
-    });
-  } catch {
-    bot.sendMessage(chatId, 'Oops, there is an error!');
+    const data = await axios.get('https://api.tinkoff.ru/v1/currency_rates?from=EUR&to=RUB');
+    const eurRate = await data.data.payload.rates[5].buy;
+    return eurRate
+  } catch (err) {
+    await bot.sendMessage(chatId, 'Oops, there is an error! Please try again later');
+    console.log(err);
   }
 }
 
-checkEUR();
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  if (text === '/start') {
+    chatsAndRates[chatId] = Infinity;
+    await bot.sendMessage(chatId, 'Это бот для мониторинга курса евро (coming soon: доллары). Введите пороговое значение и бот сообщит вам, если курс его достигнет. Курс проверяется каждые две минуты (в дальнейшем планиурую настраивать это время).');
+    return
+  }
+  if (text === '/currentrate') {
+    await bot.sendMessage(chatId, await getCurrentRate());
+    return
+  }
+  if (text.match(/^\d+$/)) {
+    setExchangeRate(chatId, text);
+    checkEUR();
+    return
+  }
+  await bot.sendMessage(chatId, 'Такой команды бот не знает.');
+});
+
+const checkEUR = async () => {
+  try {
+    const response = await axios.get('https://api.tinkoff.ru/v1/currency_rates?from=EUR&to=RUB');
+    const eurRate = await response.data.payload.rates[5].buy;
+    for (let key in chatsAndRates) {
+      if (eurRate >= chatsAndRates[key]) {
+        bot.sendMessage(key, `TIME TO SELL! Курс превысил ${chatsAndRates[key]}.`);
+      }
+    }
+    return eurRate;
+  } catch (err) {
+    for (let key in chatsAndRates) {
+      await bot.sendMessage(key, 'Oops, there is an error! Please try again later.');
+    }
+    console.log(err);
+  }
+}
+
 setInterval(checkEUR, 1000*60*refreshTimeInMinutes);
